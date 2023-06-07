c55076ea1f89f42b81b91a576ab57c5e51488a46
waynetheisinger <wayne@mowdirect.co.uk>
2023-06-07 07:29:15 +0100

    feature: get auth token

--- cli/cli.go
+++ cli/cli.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 var (
@@ -88,6 +89,25 @@ func HandleCommandLineOptions() {
 		flag.Usage()
 		os.Exit(0)
 	}
+	
+		// Check if authToken is set
+		if authToken == "" {
+			// Prompt the user to enter the authToken
+			authToken = promptAuthToken()
+		}
 
 	// Rest of your CLI tool logic...
+}
+
+func promptAuthToken() string {
+	var authToken string
+
+	fmt.Print("Enter the authentication token for the BetterUptime API: ")
+	fmt.Scanln(&authToken)
+	fmt.Println()
+
+	// Trim any leading or trailing whitespace from the input
+	authToken = strings.TrimSpace(authToken)
+
+	return authToken
 }
