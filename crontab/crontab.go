5f4438891f5133863c927315bf3f3229d69bb12a
waynetheisinger <wayne@mowdirect.co.uk>
2023-06-09 11:02:49 +0100

    wip

--- crontab/crontab.go
+++ crontab/crontab.go
@@ -21,6 +21,82 @@ func getCrontabFilePath(crontabUser string) (string, error) {
 	return crontabFile, nil
 }
 
+// helper function to read crontab file
+func readCrontabFile(crontabFile string) ([]string, error) {
+	// Read the crontab file
+	file, err := os.Open(crontabFile)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open crontab file: %w", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var lines []string
+
+	// Iterate over each line in the crontab file
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+
+	if scanner.Err() != nil {
+		return nil, fmt.Errorf("error reading crontab file: %w", scanner.Err())
+	}
+
+	return lines, nil
+}
+
+// helper function to update a cron task
+func updateCronTask(cronTask CronTask, lines []string) ([]string, error) {
+	// Check if HeartbeatURL is set and is a correctly formatted URL
+	if _, err := url.ParseRequestURI(cronTask.HeartbeatURL); err != nil {
+		return nil, fmt.Errorf("Invalid HeartbeatURL in task '%s', skipping this task.\n", cronTask.Task)
+	}
+
+	// Construct the curl command string to append to the task
+	curlCommand := fmt.Sprintf(`curl -s -o /dev/null -w "%%{http_code}" %s`, cronTask.HeartbeatURL)
+
+	// Construct the task spec string
+	taskSpec := cronTask.Spec + " " + cronTask.Task
+
+	var updatedLines []string
+
+	// Check if the line matches the cron task
+	for _, line := range lines {
+		if strings.Contains(line, taskSpec) {
+			// Append the curl command to the task
+			line += " && " + curlCommand
+		}
+
+		updatedLines = append(updatedLines, line)
+	}
+
+	return updatedLines, nil
+}
+
+// helper function to write updated cron tasks back to the file
+func writeCronTasksToFile(crontabFile string, updatedLines []string) error {
+	// Write the updated lines back to the crontab file
+	err := ioutil.WriteFile(crontabFile, []byte(strings.Join(updatedLines, "\n")), 0644)
+	if err != nil {
+		return fmt.Errorf("failed to write updated crontab file: %w", err)
+	}
+
+	return nil
+}
+
+// helper function to reload cron system
+func reloadCronSystem() error {
+	// Reload the cron system to apply the changes
+	reloadCmd := exec.Command("service", "cron", "reload")
+	if err := reloadCmd.Run(); err != nil {
+		return fmt.Errorf("failed to reload cron system: %w", err)
+	}
+
+	fmt.Println("Cron system reloaded successfully.")
+	return nil
+}
+
 // Function to parse crontab and prompt user for approval
 func ParseAndApproveCronTasks(crontabUser string) ([]CronTask, error) {
 	// Read the crontab file
@@ -117,69 +193,41 @@ func promptApproval() bool {
 
 // Function to append curl command to crontab tasks
 func AppendCronsCommand(cronTasks []CronTask, crontabUser string) error {
-
 	// Get the crontab file path
 	crontabFile, err := getCrontabFilePath(crontabUser)
 	if err != nil {
 		return err
 	}
 
+	// Read the crontab file
+	lines, err := readCrontabFile(crontabFile)
+	if err != nil {
+		return err
+	}
+
 	// Loop through the cron tasks
 	for _, cronTask := range cronTasks {
-		// Check if HeartbeatURL is set and is a correctly formatted URL
-		if _, err := url.ParseRequestURI(cronTask.HeartbeatURL); err != nil {
-			fmt.Printf("Invalid HeartbeatURL in task '%s', skipping this task.\n", cronTask.Task)
+		// Update the cron task
+		updatedLines, err := updateCronTask(cronTask, lines)
+		if err != nil {
+			fmt.Println(err)
 			continue
 		}
 
-		// Construct the curl command string to append to the task
-		curlCommand := fmt.Sprintf(`curl -s -o /dev/null -w "%%{http_code}" %s`, cronTask.HeartbeatURL)
-
-		// Read the crontab file
-		file, err := os.Open(crontabFile)
-		if err != nil {
-			return fmt.Errorf("failed to open crontab file: %w", err)
-		}
-		defer file.Close()
-
-		scanner := bufio.NewScanner(file)
-		var updatedLines []string
-
-		// Construct the task spec string
-		taskSpec := cronTask.Spec + " " + cronTask.Task
-
-		// Iterate over each line in the crontab file
-		for scanner.Scan() {
-			line := scanner.Text()
-
-			// Check if the line matches the cron task
-			if strings.Contains(line, taskSpec) {
-				// Append the curl command to the task
-				line += " && " + curlCommand
-			}
-
-			updatedLines = append(updatedLines, line)
-		}
-
-		if scanner.Err() != nil {
-			return fmt.Errorf("error reading crontab file: %w", scanner.Err())
-		}
-
 		// Write the updated lines back to the crontab file
-		err = ioutil.WriteFile(crontabFile, []byte(strings.Join(updatedLines, "\n")), 0644)
+		err = writeCronTasksToFile(crontabFile, updatedLines)
 		if err != nil {
-			return fmt.Errorf("failed to write updated crontab file: %w", err)
+			return err
 		}
 
 		fmt.Printf("Curl command appended to cron task '%s' successfully.\n", cronTask.Task)
 	}
 
-	// Reload the cron system to apply the changes
-	reloadCmd := exec.Command("service", "cron", "reload")
-	if err := reloadCmd.Run(); err != nil {
-		return fmt.Errorf("failed to reload cron system: %w", err)
+	// Reload the cron system
+	err = reloadCronSystem()
+	if err != nil {
+		return err
 	}
 
-	fmt.Println("Cron system reloaded successfully.")
 	return nil
 }
